78c28fea20b443b38661546326367a28b9763de5
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2015-11-30 09:52:53 -0800

    progress on bestPractices.js

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,7 +8,7 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.bestPracticesAnswers = {
   globals : function() {
-    myObject = {
+    var myObject = {
       name : 'Jory'
     };
 
@@ -17,9 +17,9 @@ exports.bestPracticesAnswers = {
 
   functions : function(flag) {
     if (flag) {
-      function getValue() { return 'a'; }
+      var getValue = function() { return 'a'; }
     } else {
-      function getValue() { return 'b'; }
+      var getValue = function() { return 'b'; }
     }
 
     return getValue();
@@ -30,6 +30,6 @@ exports.bestPracticesAnswers = {
   },
 
   identity : function(val1, val2) {
-
+    return val1 === val2;
   }
 };
