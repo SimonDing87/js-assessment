80914e509d5144b958cde3ad9997ed0129c2bc8b
Simon Ding <SimonDing87@gmail.com>
2016-01-27 10:00:31 -0800

    Progress on functions.js

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return fn.apply(this, arr);
   },
 
   speak : function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   functionFunction : function(str) {
-
+    return function(str2) {
+      return str +', ' + str2;
+    }
   },
 
   makeClosures : function(arr, fn) {
-
+    
   },
 
   partial : function(fn, str1, str2) {
