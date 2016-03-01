78f811ae3189e40fd943bac7977e53c6f1d63e62
Simon Ding <SimonDing87@gmail.com>
2016-03-01 10:37:58 -0800

    Progress on functions.js

--- app/functions.js
+++ app/functions.js
@@ -12,19 +12,26 @@ exports.functionsAnswers = {
   functionFunction : function(str) {
     return function(str2) {
       return str +', ' + str2;
-    }
+    };
   },
 
   makeClosures : function(arr, fn) {
-    
+    var newArr = [];
+    arr.forEach(function(item) {
+      newArr.push(fn.bind(this, item));
+    });
+    return newArr;
   },
 
   partial : function(fn, str1, str2) {
-
+    return fn.bind(this, str1, str2);
   },
 
   useArguments : function() {
-
+    var argsArray = Array.prototype.slice.call(arguments);
+    return argsArray.reduce(function(a, b) {
+      return a + b;
+    });
   },
 
   callIt : function(fn) {
